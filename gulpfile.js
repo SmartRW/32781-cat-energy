bf0c97de097d97fcf5bbb7c65a311293fbfdcce4
SmartRW <39382405+SmartRW@users.noreply.github.com>
2018-11-01 13:13:18 +0300

    adds webp images

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,52 @@
 "use strict";
 
-var gulp = require("gulp");
-var sass = require("gulp-sass");
-var plumber = require("gulp-plumber");
-var postcss = require("gulp-postcss");
-var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const server = require('browser-sync').create();
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
 
-gulp.task("css", function () {
-  return gulp.src("source/sass/style.scss")
+gulp.task('css', function () {
+  return gulp.src('source/sass/style.scss')
     .pipe(plumber())
     .pipe(sass())
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest('source/css'))
     .pipe(server.stream());
 });
 
-gulp.task("server", function () {
+gulp.task('server', function () {
   server.init({
-    server: "source/",
+    server: 'source/',
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
-  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch('source/sass/**/*.{scss,sass}', gulp.series('css'));
+  gulp.watch('source/*.html').on('change', server.reload);
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task('start', gulp.series('css', 'server'));
+
+gulp.task('images', function () {
+  return gulp.src('source/img/**/*.{png,jpg,svg}')
+    .pipe(imagemin([
+    imagemin.optipng({ optimizationLevel: 3 }),
+    imagemin.jpegtran({ progressive: true }),
+    imagemin.svgo()
+    ]))
+    .pipe(gulp.dest('source/img'));
+});
+
+gulp.task('webp', function () {
+  return gulp.src('source/img/**/*.{png,jpg}')
+   .pipe(webp({quality: 90}))
+   .pipe(gulp.dest('source/img'));
+});
