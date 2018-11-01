397610de52c485860b022cb022cbf31db2e43372
SmartRW <39382405+SmartRW@users.noreply.github.com>
2018-11-01 13:16:19 +0300

    bug fix

--- gulpfile.js
+++ gulpfile.js
@@ -47,6 +47,6 @@ gulp.task('images', function () {
 
 gulp.task('webp', function () {
   return gulp.src('source/img/**/*.{png,jpg}')
-   .pipe(webp({quality: 90}))
-   .pipe(gulp.dest('source/img'));
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest('source/img'));
 });
